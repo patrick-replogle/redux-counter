df72a38f2856cb7bf3a190b2360124fafb855005
patrick-replogle <patrickr1138@gmail.com>
2020-03-31 11:45:04 -0700

    followed along with lecture than reorganized code and file structure

---
+++ src/actions/index.js
@@ -0,0 +1,15 @@
+export const INCREMENT = 'INCREMENT';
+export const DECREMENT = 'DECREMENT';
+export const RESET = 'RESET';
+
+export const incrementValue = () => ({
+  type: INCREMENT,
+});
+
+export const decrementValue = () => ({
+  type: DECREMENT,
+});
+
+export const resetValue = () => ({
+  type: RESET,
+});
--- src/index.js
+++ src/index.js
@@ -1,21 +1,43 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
-
+import { createStore } from 'redux';
+import { connect, Provider } from 'react-redux';
+import { reducer } from './reducers/index.js';
+import { incrementValue, decrementValue, resetValue } from './actions/index.js';
 import './styles.scss';
+
+const store = createStore(reducer);
 
 class Counter extends Component {
   render() {
+    const { count, incrementValue, decrementValue, resetValue } = this.props;
     return (
       <main className="Counter">
-        <p className="count">0</p>
+        <p className="count">{count}</p>
         <section className="controls">
-          <button>Increment</button>
-          <button>Decrement</button>
-          <button>Reset</button>
+          <button onClick={incrementValue}>Increment</button>
+          <button onClick={decrementValue}>Decrement</button>
+          <button onClick={resetValue}>Reset</button>
         </section>
       </main>
     );
   }
 }
 
-render(<Counter />, document.getElementById('root'));
+const mapStateToProps = state => {
+  return state;
+};
+const mapDispatchToProps = {
+  incrementValue,
+  decrementValue,
+  resetValue,
+};
+
+const CounterContainer = connect(mapStateToProps, mapDispatchToProps)(Counter);
+
+render(
+  <Provider store={store}>
+    <CounterContainer />
+  </Provider>,
+  document.getElementById('root'),
+);
